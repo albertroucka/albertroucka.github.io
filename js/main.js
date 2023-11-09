23db55c1b995516c6d45a5e8774649148ba4f94d
albertroucka <81353207+albertroucka@users.noreply.github.com>
2023-11-09 22:34:34 +0100

    Verze 0.4

--- js/main.js
+++ js/main.js
@@ -2,45 +2,85 @@ const elementMap = document.getElementById("custom-map-container");
 const elementPin = document.getElementById("custom-btn-container");
 const elementCorrect = document.getElementById("custom-btn-container2");
 const elementDescription = document.getElementById("map-desctiption");
+const divTextAnswer = document.getElementById("custom-text-input");
 const elementTextAnswer = document.getElementById("location-input-text");
 
-var currentLocation; var state;
+const players = []; var currentLocation; var state; var round; var score; const type = 0;
 
 class Location {
-    constructor(name, coordinateX, coordinateY, description) {
+    constructor(name, type, coordinateX, coordinateY, description) {
       this.name = name;
+      this.type = type;
       this.coordinateX = coordinateX;
       this.coordinateY = coordinateY;
       this.description = description;
     }
 }
 
+class Player {
+    constructor(name, score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
 function getAllLocations()
 {
     return [
-        new Location("Strakonice", 49.26950, 13.90641, "Strakonice jsou město na soutoku Otavy a Volyňky. Žije zde 22 583 obyvatel. Na rozdíl od mnoha měst v Čechách a na Moravě nebyly Strakonice založeny jako královské město, ale vznikaly postupným slučováním čtyř menších osad: Strakonic, Bezděkova, Žabokrt a Lomu, které se spojily v poddanské město Strakonice."), 
-        new Location("Kralupy nad Vltavou", 50.24169, 14.31016, ""), 
-        new Location("Hrad Kost", 50.49020, 15.13520, "Zachovalý pevný gotický hrad, jehož založení se datuje do 2. poloviny 14. století. Po třicetileté válce unikl osudu jiných hradů určených k likvidaci, od poloviny 18. století však pustnul. Hrad patří k těm, jejichž výrazná silueta se nezapomíná, na Kosti především díky typické čtyřhranné věži."), 
-        new Location("Dolní Morava", 50.12242, 16.79969, "Dolní Morava je malá obec s 421 obyvateli. Obec je známým horským lyžařským střediskem. Část obce náleží do Národní přírodní rezervace Králický Sněžník, a právě ten je dominantou zdejšího kraje. Od roku 2022 se také v její blízkosti nachází nejdelší vistutý most pro pěší na světe."), 
-        new Location("Havířov", 49.78049, 18.43073, ""), 
-        new Location("Lednice", 48.80002, 16.80350, "Lednice je obec se 2255 obyvateli, která je ze severu obtékána řekou Zámeckou Dyjí. Zdejší lichtenštejnský zámek s rozsáhlým parkem patří mezi nejvyhledávanější turistické destinace v celém Česku, spolu s Valticemi je centrem Lednicko-valtického areálu."),
-        new Location("Propast Macocha", 49.37324, 16.72982, "Propast hluboká 138 metrů vznikla zřícením stropu velké jeskyně. Její horní část je dlouhá 174 metrů a široká 76 metrů. Jako první sestoupil do propasti mnich Lazar Schopper v roce 1723. Na okraji propasti najdete dva vyhlídkové můstky. Na dně se nachází významná botanická lokalita."),
-        new Location("Luhačovice", 49.09982, 17.75747, "Lázeňské město leží v malebném údolí chráněné krajinné oblasti Bílé Karpaty. Unikátní léčivé prameny činí z Luhačovic špičkové léčebné lázně v celoevropském měřítku. Specializují se na lázeňskou léčbu onemocnění dýchacích cest. Nejznámějším léčivým pramenem je Vincentka."),
-        new Location("Sněžka", 50.73597, 15.73966, "Sněžka se nachází ve východní části Krkonoš na Hraničním hřebenu. Přes její vrchol prochází česko-polská státní hranice. První v historii zaznamenaný výstup je z roku 1456, kdy jistý Benátčan hledal v horách drahé kamení. Název Sněžka pochází z 19. století, je odvozen od pojmenování Sněžná – jako „sněhem pokrytá“."),
-        new Location("Máchovo jezero", 50.58349, 14.64984, "Největší umělá vodní plocha (278 ha) Máchova kraje byla založena jako Velký rybník císařem Karlem IV. v roce 1367. Nachází se v nadmořské výšce 266 m. Díky krajině, ve které je zasazeno, je dnes vyhledávaným turistickým cílem.")];
+        new Location("Strakonice", "Město", 49.26950, 13.90641, "Strakonice jsou město na soutoku Otavy a Volyňky. Žije zde 22 583 obyvatel. Na rozdíl od mnoha měst v Čechách a na Moravě nebyly Strakonice založeny jako královské město, ale vznikaly postupným slučováním čtyř menších osad: Strakonic, Bezděkova, Žabokrt a Lomu, které se spojily v poddanské město Strakonice."), 
+        new Location("Kralupy nad Vltavou", "Město", 50.24169, 14.31016, "Bude doplněno."), 
+        new Location("Hrad Kost", "Hrad", 50.49020, 15.13520, "Zachovalý pevný gotický hrad, jehož založení se datuje do 2. poloviny 14. století. Po třicetileté válce unikl osudu jiných hradů určených k likvidaci, od poloviny 18. století však pustnul. Hrad patří k těm, jejichž výrazná silueta se nezapomíná, na Kosti především díky typické čtyřhranné věži."), 
+        new Location("Dolní Morava", "Obec", 50.12242, 16.79969, "Dolní Morava je malá obec s 421 obyvateli. Obec je známým horským lyžařským střediskem. Část obce náleží do Národní přírodní rezervace Králický Sněžník, a právě ten je dominantou zdejšího kraje. Od roku 2022 se také v její blízkosti nachází nejdelší vistutý most pro pěší na světe."), 
+        new Location("Havířov", "Město", 49.78049, 18.43073, "Bude doplněno."), 
+        new Location("Lednice", "Obec",48.80002, 16.80350, "Lednice je obec se 2255 obyvateli, která je ze severu obtékána řekou Zámeckou Dyjí. Zdejší lichtenštejnský zámek s rozsáhlým parkem patří mezi nejvyhledávanější turistické destinace v celém Česku, spolu s Valticemi je centrem Lednicko-valtického areálu."),
+        new Location("Propast Macocha", "", 49.37324, 16.72982, "Propast hluboká 138 metrů vznikla zřícením stropu velké jeskyně. Její horní část je dlouhá 174 metrů a široká 76 metrů. Jako první sestoupil do propasti mnich Lazar Schopper v roce 1723. Na okraji propasti najdete dva vyhlídkové můstky. Na dně se nachází významná botanická lokalita."),
+        new Location("Luhačovice", "Město", 49.09982, 17.75747, "Lázeňské město leží v malebném údolí chráněné krajinné oblasti Bílé Karpaty. Unikátní léčivé prameny činí z Luhačovic špičkové léčebné lázně v celoevropském měřítku. Specializují se na lázeňskou léčbu onemocnění dýchacích cest. Nejznámějším léčivým pramenem je Vincentka."),
+        new Location("Sněžka", "Hora", 50.73597, 15.73966, "Sněžka se nachází ve východní části Krkonoš na Hraničním hřebenu. Přes její vrchol prochází česko-polská státní hranice. První v historii zaznamenaný výstup je z roku 1456, kdy jistý Benátčan hledal v horách drahé kamení. Název Sněžka pochází z 19. století, je odvozen od pojmenování Sněžná – jako „sněhem pokrytá“."),
+        new Location("Máchovo jezero", "Jezero", 50.58349, 14.64984, "Největší umělá vodní plocha (278 ha) Máchova kraje byla založena jako Velký rybník císařem Karlem IV. v roce 1367. Nachází se v nadmořské výšce 266 m. Díky krajině, ve které je zasazeno, je dnes vyhledávaným turistickým cílem.")
+    ];
 }
 
+//Spuštění/začátek
+players.push(new Player("Hráč 1", 0));
+players.forEach(refreshPlayer);
 const locations = getAllLocations();
 locations.forEach(element => { console.log(element) });
-
 $('.custom-btn-container').draggable();
+round = 0;
 startRound();
+getCurrentBrowserZoom();
+getCurrentBrowserSize();
+//konec začátku
 
 function startRound()
 {
     currentLocation = locations[Math.floor(Math.random()*locations.length)];
-    flashingLetters(currentLocation.name);
-    //showAllLetters(mixedLetters(currentLocation.name));
+    round++; elementMap.textContent = "Kolo " + round;
+    divTextAnswer.style.visibility = "visible";
+    $('.custom-btn-container').draggable("enable");
+
+    if (type == 0) {
+        showAllLetters(mixedLetters(currentLocation.name));
+    } else {
+        flashingLetters(currentLocation.name);
+    }
+}
+
+function refreshPlayer(item, index)
+{
+    let playerName= document.getElementById("player" + index + "-nickname");
+    let playerScore = document.getElementById("player" + index + "-score");
+    console.log("player" + index + "-nickname");
+    playerName.textContent = item.name;
+    playerScore.textContent = item.score;
+}
+
+function changePlayerScore(playerID, score)
+{
+    let playerScore = document.getElementById("player" + playerID + "-score");
+    playerScore.textContent = parseInt(playerScore.textContent) + score;
+    players[playerID].score = parseInt(playerScore.textContent)
+    players[playerID] = new Player(players[playerID].name, playerScore.textContent);
 }
 
 function showAllLetters(location)
@@ -165,9 +205,23 @@ function showAllLettersColor(location)
     }
 }
 
-function delay(time) {
+function cleanAllLetters(location)
+{
+    let i = 0;
+
+    while(i < location.length)
+    {
+        let letter = document.getElementById("letter" + i);
+        letter.textContent = null;
+        letter.style.visibility = "hidden";     
+        i++;
+    }
+}
+
+function delay(time) 
+{
     return new Promise(resolve => setTimeout(resolve, time));
-  }
+}
 
 function checkLocationGuessAnswer()
 {
@@ -177,8 +231,11 @@ function checkLocationGuessAnswer()
     if (elementTextAnswer.value.toUpperCase() == currentLocation.name.toUpperCase())
     {
         state = "guessed";
+        changePlayerScore(0, 10);
+        divTextAnswer.style.visibility = "hidden";
         //elementMap.textContent = currentLocation.name;
         elementDescription.textContent = currentLocation.description;
+        elementDescription.style.visibility = "visible";
         elementPin.style.visibility = "visible";
         showAllLettersColor(currentLocation.name);
         showAllLetters(currentLocation.name);
@@ -186,7 +243,10 @@ function checkLocationGuessAnswer()
     else
     {
         console.log("Špatná odpověď!");
+        alert("Špatná odpověď");
     }
+
+    elementTextAnswer.value = null;
 }
 
 elementTextAnswer.addEventListener("keypress", function(event) {
@@ -197,7 +257,20 @@ elementTextAnswer.addEventListener("keypress", function(event) {
       // Trigger the button element with a click
       document.getElementById("location-input-submit").click();
     }
-  });
+});
+
+function elementPinDoubleClick() {
+
+    if (state == "finished")
+    {
+        finishRound();
+    }
+    else
+    {
+        checkMapPositionAnswer();
+    }
+}
+
 
 function checkMapPositionAnswer() 
 {
@@ -210,11 +283,14 @@ function checkMapPositionAnswer()
     elementCorrect.style.left = coordinates[0] + 'px';
     elementCorrect.style.top = coordinates[1]  + 'px';
     const distance = getDistance(pinX, pinY, coordinates[0], coordinates[1]);
-    console.log(distance);
+    console.log("Vzdálenost: " + distance);
 
-    if(distance < 40)
+    if(distance < 53)
     {
+        changePlayerScore(0, 40);
         elementCorrect.style.background = '#00ff00';
+        $('.custom-btn-container').draggable("disable");
+        //elementPin.setAttribute("draggable", "false");
     }
     else
     {
@@ -222,6 +298,7 @@ function checkMapPositionAnswer()
     }
 
     elementCorrect.style.visibility = "visible";
+    state = "finished";
 }
 
 function getPinPosition()
@@ -263,9 +340,52 @@ function convertCoordinatesToMap(coordinateX, coordinateY)
     return [d2, d1];
 }
 
-function getDistance(x1, y1, x2, y2){
+function getDistance(x1, y1, x2, y2)
+{
     let y = x2 - x1;
     let x = y2 - y1;
     
     return Math.sqrt(x * x + y * y);
+}
+
+
+function getCurrentBrowserZoom() 
+{
+    let zoom = Math.round(window.devicePixelRatio*100);
+    console.log("Zoom: " + zoom + "%");
+    return zoom;
+}
+
+function getCurrentBrowserSize() 
+{
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    console.log("Browser window size: " + w + "x" + h + "px");
+    return[w, h]
+}
+
+function finishRound()
+{
+    elementPin.style.visibility = "hidden";
+    elementDescription.style.visibility = "hidden";
+    elementPin.style.top = "135px"; elementPin.style.left = "10px";
+    elementCorrect.style.visibility = "hidden";
+    elementDescription.textContent = null;
+    cleanAllLetters(currentLocation.name);
+
+    if (round < 3)
+    {
+        startRound();
+    }
+    else
+    {
+        finishGame();
+    }
+}
+
+function finishGame()
+{
+    state = "end";
+    console.log(players[0]);
+    alert("Konec hry " + players[0].name + "! Celkové skóre je " + players[0].score + "!");
 }
